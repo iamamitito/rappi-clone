228d0e8baafe843ce69c73dbb3abbd23b189a88d
Héctor Espinoza <hespinoza@uchile.cl>
2020-09-30 14:37:49 -0300

    cvs file imported

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Papa from 'papaparse';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [stock, setStock] = useState([]);
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch('/stock.csv')
+      const reader = response.body.getReader()
+      const result = await reader.read() // raw array
+      const decoder = new TextDecoder('utf-8')
+      const csv = decoder.decode(result.value) // the csv text
+      const results = Papa.parse(csv, { header: true }) // object with { data, errors, meta }
+      const stock = results.data // array of objects
+      setStock(stock);
+    }
+    getData()
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {stock.map(product => <h2 key={product.Id}>{product.Nombre}</h2>)}
     </div>
   );
 }
